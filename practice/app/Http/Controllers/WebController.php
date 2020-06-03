2ce4c2beb21652302d0741f847faca81b089aa96
nguyencong12610 <53223955+nguyencong12610@users.noreply.github.com>
2020-06-03 11:15:59 +0700

    111

---
+++ practice/app/Http/Controllers/WebController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
+
+class WebController extends Controller
+{
+    public function index()
+    {
+        return view("home");
+
+    }
+}
